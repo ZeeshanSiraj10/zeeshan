237f7aa52f3f8bae6def40af7db90b5af9bc54cf
umer ejaz <107603379+Palladion44@users.noreply.github.com>
2023-04-27 16:03:55 +0500

    hemllo gg

---
+++ Wasif (E project)/Images/Contact.js
@@ -0,0 +1,8 @@
+var na = document.getElementById("Name")
+let Email= document.getElementById("Email")
+let Password= document.getElementById("Password")
+let Contact= document.getElementById("Contact")
+let Feedback= document.getElementById("Feedback")
+
+
+localStorage.setItem("Name",[na])
